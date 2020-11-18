a7ef4804822eca837d66b6edc7f9d9a3cfb5ef2f
DmitryErofeev <61731744+DmitryErofeev@users.noreply.github.com>
2020-11-18 13:44:45 +0300

    Update __init__.py

--- netmiko/eltex/__init__.py
+++ netmiko/eltex/__init__.py
@@ -1,4 +1,5 @@
 from netmiko.eltex.eltex_ssh import EltexSSH
+from netmiko.eltex.eltex_ssh import EltexTelnet
 from netmiko.eltex.eltex_esr_ssh import EltexEsrSSH
 
 __all__ = ["EltexSSH", "EltexEsrSSH", "EltexTelnet"]
